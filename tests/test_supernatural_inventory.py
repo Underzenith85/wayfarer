c8916aad429f72b812d8a6fadac6b9e468fcb1a1
Chang Liu <c.liu@rippling.com>
2026-09-12 02:17:42 -0700

    Complete GURPS water spell coverage (#231)

--- tests/test_supernatural_inventory.py
+++ tests/test_supernatural_inventory.py
@@ -151,6 +151,7 @@ def test_every_entry_has_concrete_runtime_and_source_blockers_and_real_evidence(
                 228,
                 229,
                 230,
+                231,
                 233,
                 234,
                 235,
