e70726cd81883bd7d9f3c27a576bbf39111ff7cd
Chang Liu <spiritualmachines@gmail.com>
2026-09-12 02:29:14 -0700

    Complete GURPS healing spell coverage (#224) (#548)

--- tests/test_supernatural_inventory.py
+++ tests/test_supernatural_inventory.py
@@ -144,6 +144,7 @@ def test_every_entry_has_concrete_runtime_and_source_blockers_and_real_evidence(
                 221,
                 222,
                 223,
+                224,
                 233,
                 234,
                 235,
